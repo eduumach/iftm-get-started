45350286bf806358c1b1ec14eceb11d37d92805f
Eduardo M. Rezende <eduardomachadorezende0@gmail.com>
2025-02-18 16:55:01 -0300

    feat: adding moradia and resturante

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const logo = document.querySelector('.logo');
-    const easterEgg = document.querySelector('.easter-egg');
+function mostrarMapa(lat, lng) {
+    const mapa = document.getElementById('mapa');
+    const url = `https://www.google.com/maps?q=${lat},${lng}&z=20&output=embed`;
 
-    logo.addEventListener('click', () => {
-        easterEgg.style.display = 'block';
-        setTimeout(() => {
-            easterEgg.style.display = 'none';
-        }, 3000);
+    mapa.src = url;
+
+    document.getElementById('mapa-container').scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
     });
+}
+
+
+document.querySelector('.logo').addEventListener('click', function() {
+    document.querySelector('.easter-egg').style.display = 'block';
+    setTimeout(() => {
+        document.querySelector('.easter-egg').style.display = 'none';
+    }, 3000);
 });
